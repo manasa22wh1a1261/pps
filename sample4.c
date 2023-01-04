22cff633d33e511428739a6d1bce731edd95dd4d
manasa22wh1a1261 <121919508+manasa22wh1a1261@users.noreply.github.com>
2023-01-04 10:21:48 +0530

    Add files via upload

---
+++ sample4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+{  int x1; 
+   int x2; 	 
+   int y1;   	
+   int y2;  
+   printf("enter the value of x1,x2,y1,y2);
+   scanf("%d%d%d%d",&x1,&x2,&y1&y2);
+  distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+  printf("%d\n"distance); 
+}  
---
+++ while.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,count;
+	printf("enter the range");
+	scanf("%d",&n);
+	while(i=1;i<=n;i++)
+	{
+		count=0
+			printf("prime");
+	}
